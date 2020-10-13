dc452b8677bfaa5f2fa380752ff6105c9701716e
Seekerzero <1354052613@qq.com>
2020-10-13 02:39:27 -0400

    Adding test for json handler. Found a bug for getting entries.

--- app/src/main/java/com/example/feedme/JsonHandler.kt
+++ app/src/main/java/com/example/feedme/JsonHandler.kt
@@ -9,36 +9,29 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 private const val TAG = "JSON_HANDLER"
+private const val NumOfAwards = 9
 
 class JsonHandler(){
-
     val mochiInfo = MochiInfo("test", SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().time))
-    var awardDate1: String = "00000000"// for what award
-    var awardDate2: String = "00000000"
-    var awardDate3: String = "00000000"
-    var awardDate4: String = "00000000"
-    var awardDate5: String = "00000000"
-    var awardDate6: String = "00000000"
-    var awardDate7: String = "00000000"
-    var awardDate8: String = "00000000"
-    var awardDate9: String = "00000000"
+    var awardDates = mutableListOf<String>()
+    var awardNames = mutableListOf<String>()
 
 
+    init {
+        for(i in 0 until NumOfAwards){
+            awardDates.add("00000000")
+        }
+       print("Finished Initialized award date list with size of ${awardDates.size}\n")
+    }
 
 
     fun createMochiInfoFile(context: Context){
         val jsonObject = JSONObject()
         jsonObject.put("Name", mochiInfo.name)
         jsonObject.put("Birthday", mochiInfo.birthday)
-        jsonObject.put("AwardDate1", awardDate1)
-        jsonObject.put("AwardDate2", awardDate2)
-        jsonObject.put("AwardDate3", awardDate3)
-        jsonObject.put("AwardDate4", awardDate4)
-        jsonObject.put("AwardDate5", awardDate5)
-        jsonObject.put("AwardDate6", awardDate6)
-        jsonObject.put("AwardDate7", awardDate7)
-        jsonObject.put("AwardDate8", awardDate8)
-        jsonObject.put("AwardDate9", awardDate9)
+        for(i in 0 until awardDates.size){
+            jsonObject.put("AwardDate${i+1}", awardDates[i])
+        }
         val mochiInfoString = jsonObject.toString()
         val file = File(context.filesDir, "MochiInfo.json")
         val filewriter = FileWriter(file)
@@ -58,54 +51,41 @@ class JsonHandler(){
         val jsonObject = JSONObject(line)
         mochiInfo.name = jsonObject.get("Name").toString()
         mochiInfo.birthday = jsonObject.get("Birthday").toString()
-        awardDate1 = jsonObject.get("AwardDate1").toString()
-        awardDate2 = jsonObject.get("AwardDate2").toString()
-        awardDate3 = jsonObject.get("AwardDate3").toString()
-        awardDate4 = jsonObject.get("AwardDate4").toString()
-        awardDate5 = jsonObject.get("AwardDate5").toString()
-        awardDate6 = jsonObject.get("AwardDate6").toString()
-        awardDate7 = jsonObject.get("AwardDate7").toString()
-        awardDate8 = jsonObject.get("AwardDate8").toString()
-        awardDate9 = jsonObject.get("AwardDate9").toString()
-    }
-
-    @SuppressLint("SimpleDateFormat")
-    private fun convertStr2Date(strDate: String): Date {
-        if (strDate == null){
-            return SimpleDateFormat("yyyyMMdd").parse("00000000")
+        var newAwardDates = mutableListOf<String>()
+        for (i in 0 until  awardDates.size) {
+             awardDates[i] = jsonObject.get("AwardDate${i+1}").toString()
         }
-        return SimpleDateFormat("yyyyMMdd").parse(strDate)
     }
 
-    fun getDateforAward(awardName: String):Date{
+    fun getAwardDate(awardName: String):String{
                 return when(awardName){
-                    "Happy Chomper" -> convertStr2Date(awardDate1)
-                    "Healthy Chomper" -> convertStr2Date(awardDate2)
-                    "Strong & Healthy Chomper" -> convertStr2Date(awardDate3)
-                    "Healthy Heart" -> convertStr2Date(awardDate4)
-                    "Health Fighter" -> convertStr2Date(awardDate5)
-                    "Health Warrior" -> convertStr2Date(awardDate6)
-                    "World Travller" -> convertStr2Date(awardDate7)
-                    "Extreme Health" -> convertStr2Date(awardDate8)
-                    "They Grow Up So Fast" -> convertStr2Date(awardDate9)
-                    else -> convertStr2Date("00000000")
-        }
+                    "Happy Chomper" -> awardDates[0]
+                    "Healthy Chomper" -> awardDates[1]
+                    "Strong & Healthy Chomper" -> awardDates[2]
+                    "Healthy Heart" -> awardDates[3]
+                    "Health Fighter" -> awardDates[4]
+                    "Health Warrior" -> awardDates[5]
+                    "World Travller" -> awardDates[6]
+                    "Extreme Health" -> awardDates[7]
+                    "They Grow Up So Fast" -> awardDates[8]
+                    else ->"00000000"
+                }
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun updateDateforAward(context: Context, awardName: String, date: Date){
+    fun updateAwardDate(awardName: String, dateString: String){
         when(awardName){
-            "Happy Chomper" -> awardDate1 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Healthy Chomper" -> awardDate2 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Strong & Healthy Chomper" -> awardDate3 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Healthy Heart" -> awardDate4 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Health Fighter" -> awardDate5 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Health Warrior" -> awardDate6 = SimpleDateFormat("yyyyMMdd").format(date)
-            "World Travller" -> awardDate7 = SimpleDateFormat("yyyyMMdd").format(date)
-            "Extreme Health" -> awardDate8 = SimpleDateFormat("yyyyMMdd").format(date)
-            "They Grow Up So Fast" -> awardDate9 = SimpleDateFormat("yyyyMMdd").format(date)
+            "Happy Chomper" -> awardDates[0] = dateString
+            "Healthy Chomper" -> awardDates[1] = dateString
+            "Strong & Healthy Chomper" -> awardDates[2] = dateString
+            "Healthy Heart" -> awardDates[3] = dateString
+            "Health Fighter" -> awardDates[4] = dateString
+            "Health Warrior" -> awardDates[5] = dateString
+            "World Travller" -> awardDates[6] = dateString
+            "Extreme Health" -> awardDates[7] = dateString
+            "They Grow Up So Fast" -> awardDates[8] = dateString
+            else -> print("Wrong Award Name provided\n")
         }
-        createMochiInfoFile(context)
     }
 
 
--- app/src/test/java/com/example/feedme/JsonHandlerTest.kt
+++ app/src/test/java/com/example/feedme/JsonHandlerTest.kt
@@ -1,12 +1,49 @@
 package com.example.feedme
 
+
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
+
 
 class JsonHandlerTest {
 
+    private lateinit var jsonHandler: JsonHandler
+
     @Before
     fun setUp() {
+        jsonHandler = JsonHandler()
+        jsonHandler.awardDates[0] ="20201010"
+        jsonHandler.awardDates[1] ="20201011"
     }
+
+    @Test
+    fun testGetAwardDateCorrectInput(){
+        assertEquals(jsonHandler.getAwardDate("Happy Chomper"), "20201010")
+    }
+
+    @Test
+    fun testGetAwardDateInCorrectInput(){
+        assertEquals(jsonHandler.getAwardDate("wrong award"), "00000000")
+    }
+
+    @Test
+    fun testupdateAwardDateCorrectInput(){
+        jsonHandler.updateAwardDate("Happy Chomper", "19980802")
+        assertEquals(jsonHandler.awardDates[0], "19980802")
+    }
+
+    @Test
+    fun testupdateAwardDateInorrectInput(){
+        jsonHandler.updateAwardDate("wrong award", "19491001")
+        assertEquals(jsonHandler.awardDates[0], "20201010")
+        assertEquals(jsonHandler.awardDates[1], "20201011")
+        for (i in 2 until jsonHandler.awardDates.size){
+            assertEquals(jsonHandler.awardDates[i], "00000000")
+        }
+    }
+
+
+
 }
